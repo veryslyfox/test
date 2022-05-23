2fd04317059bbf2694046593ad2203e7af944d81
Avgust Zagorodnikov <zagavg@yandex.ru>
2022-05-23 11:16:13 +0300

    Complete

---
+++ Compiler.cs
@@ -0,0 +1,31 @@
+static class Compiler
+{
+    public static void Compile(string code)
+    {
+        List<string> expressions = new();
+        string currentExpression = "";
+        foreach (var item in code)
+        {
+            if (item == ';')
+            {
+                expressions.Add(currentExpression);
+                currentExpression = "";
+            }
+            currentExpression += item;
+        }
+        for (Reference<int> i = 0; i < expressions.Count; i++)
+        {
+            var expression = expressions[i];
+            if (expression.AsSpan(0, 4) == "goto")
+            {
+                if (int.TryParse(expression.AsSpan(5), out var a))
+                {
+                    i = a;
+                }
+            }
+            if (expression.AsSpan(0, 5) == "print")
+            {
+            }
+        }
+    }
+}
